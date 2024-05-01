61623c6764daa5082fe9ebb7ed8fde9998cb44d4
BelkyDan <belchordaniel123@gmail.com>
2024-05-01 11:07:36 -0300

    Criação do userRepository, database seeding e injeção de dependencia

---
+++ src/main/java/com/educando/curso2/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.educando.curso2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educando.curso2.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+}	
