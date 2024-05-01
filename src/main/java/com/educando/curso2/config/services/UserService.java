3001a1be9e139eef6867bf41ba6f2f85379a53d9
BelkyDan <belchordaniel123@gmail.com>
2024-05-01 11:27:14 -0300

    componente registrado e serviços

---
+++ src/main/java/com/educando/curso2/config/services/UserService.java
@@ -0,0 +1,28 @@
+package com.educando.curso2.config.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educando.curso2.entities.User;
+import com.educando.curso2.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
