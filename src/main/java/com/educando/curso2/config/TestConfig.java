6779adcad8ab771c29b53ab94ab58c6c7b01c6a2
BelkyDan <belchordaniel123@gmail.com>
2024-05-01 19:28:29 -0300

    OneToOne de payment/Order

--- src/main/java/com/educando/curso2/config/TestConfig.java
+++ src/main/java/com/educando/curso2/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educando.curso2.entities.Category;
 import com.educando.curso2.entities.Order;
 import com.educando.curso2.entities.OrderItem;
+import com.educando.curso2.entities.Payment;
 import com.educando.curso2.entities.Product;
 import com.educando.curso2.entities.User;
 import com.educando.curso2.entities.enums.OrderStatus;
@@ -77,5 +78,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
