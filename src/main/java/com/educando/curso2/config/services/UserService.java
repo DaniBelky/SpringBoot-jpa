460576d0dae7ca3f0f653586ae52459fd9be9a76
BelkyDan <belchordaniel123@gmail.com>
2024-05-01 20:40:04 -0300

    Tratamento de exceção

--- src/main/java/com/educando/curso2/config/services/UserService.java
+++ src/main/java/com/educando/curso2/config/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.educando.curso2.config.services.exception.ResourceNotFoundException;
 import com.educando.curso2.entities.User;
 import com.educando.curso2.repositories.UserRepository;
 
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
