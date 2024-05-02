ead78ed58ea730f7cebc1d547cca9c6be3cfa81c
BelkyDan <belchordaniel123@gmail.com>
2024-05-01 21:03:43 -0300

    Tratamento de Exceção - update

--- src/main/java/com/educando/curso2/config/services/UserService.java
+++ src/main/java/com/educando/curso2/config/services/UserService.java
@@ -13,6 +13,8 @@ import com.educando.curso2.config.services.exception.ResourceNotFoundException;
 import com.educando.curso2.entities.User;
 import com.educando.curso2.repositories.UserRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
